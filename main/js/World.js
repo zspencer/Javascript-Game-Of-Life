3e90849f09f0daa7380b62c3fd7efc6330d2f527
Zachary Spencer <zspencer@zacharyspencer.com>
2010-10-09 17:05:09 -0400

    Implemented a visitor pattern for use in countNeighbors and getNeighbors to remove duplicate loops.

--- main/js/World.js
+++ main/js/World.js
@@ -1,15 +1,16 @@
 require.def(function(){
-	var WorldObject = function() {}
+    var WorldObject = function(){
+    }
     var prototype = {
         spawn: function(x, y){
-            this.setCell(x,y,true);
+            this.setCell(x, y, true);
         },
-		kill: function(x,y) {
-			this.setCell(x,y, false);
-		},
+        kill: function(x, y){
+            this.setCell(x, y, false);
+        },
         isPopulatedAt: function(x, y){
             this.initializeRow(x);
-            return (!this.cells[x][y]) ? false : true; 
+            return (!this.cells[x][y]) ? false : true;
         },
         initializeRow: function(x){
             if (this.cells[x] == null) {
@@ -18,82 +19,86 @@ require.def(function(){
         },
         cellShouldLive: function(x, y){
             var neighborCount = this.countNeighbors(x, y);
-            if (neighborCount < 2 || neighborCount > 3) { return false; }
-			if (neighborCount == 3) { return true;}
-            return this.isPopulatedAt(x,y);
+            if (neighborCount < 2 || neighborCount > 3) {
+                return false;
+            }
+            if (neighborCount == 3) {
+                return true;
+            }
+            return this.isPopulatedAt(x, y);
         },
-		evolve: function() {
-			var newWorld = WorldObject.create();
-			this.trace();
-			var world = this;
-			jQuery.each(world.cells, function(x, row){
+        evolve: function(){
+            var newWorld = WorldObject.create();
+            this.trace();
+            var world = this;
+            jQuery.each(world.cells, function(x, row){
                 jQuery.each(row, function(y, lives){
                     if (world.cellShouldLive(x, y)) {
-						newWorld.spawn(x, y);
-					}
+                        newWorld.spawn(x, y);
+                    }
                 });
             });
-			this.cells = newWorld.cells;
-		},
-		getNeighbors: function(x, y) {
-			x = parseInt(x); y = parseInt(y);
-			var neighbors = WorldObject.create();
-			var currentX = x-1;
-			while(currentX<=x+1) {
-				var currentY = y-1;
-				while(currentY<=y+1) {
-					neighbors.setCell(currentX,currentY,this.isPopulatedAt(currentX, currentY));
-					currentY++;
-				}
-				currentX++;
-			}
-			return neighbors.cells;
-		},
-		setCell: function(x,y,life) {
-			this.initializeRow(x);
-			this.cells[x][y]= life;
-		},
-		cellsAreAtTheSameLocation: function(x1,y1,x2,y2) {
-			return (x1==x2 && y1 == y2);
-		},
-		shouldIncrementNeighborCount: function(x,y, currentX, currentY) {
-			return !this.cellsAreAtTheSameLocation(x,y, currentX, currentY) && this.isPopulatedAt(currentX, currentY)
-		},
+            this.cells = newWorld.cells;
+        },
+        getNeighbors: function(x, y){
+            var neighbors = WorldObject.create();
+			var self = this;
+            this.visitAdjacentCells(x, y, function(adjacentX, adjacentY){
+                neighbors.setCell(adjacentX, adjacentY, self.isPopulatedAt(adjacentX, adjacentY));
+            });
+            return neighbors.cells;
+        },
+        setCell: function(x, y, life){
+            this.initializeRow(x);
+            this.cells[x][y] = life;
+        },
+        cellsAreAtTheSameLocation: function(x1, y1, x2, y2){
+            return (x1 == x2 && y1 == y2);
+        },
+        shouldIncrementNeighborCount: function(x, y, currentX, currentY){
+            return !this.cellsAreAtTheSameLocation(x, y, currentX, currentY) && this.isPopulatedAt(currentX, currentY)
+        },
+        visitAdjacentCells: function(x, y, callback){
+            x = parseInt(x); y = parseInt(y);
+            var currentX = x - 1;
+            while (currentX <= x + 1) {
+                var currentY = y - 1;
+                while (currentY <= y + 1) {
+                    callback(currentX, currentY);
+                    currentY++;
+                }
+                currentX++;
+            }
+        },
         countNeighbors: function(x, y){
-			x = parseInt(x); y = parseInt(y);
-			var count = 0;
-			var currentX = x - 1; 
-			while (currentX <= x + 1) {
-				var currentY = y -1;
-				while (currentY <= y + 1) {
-					if (this.shouldIncrementNeighborCount(x,y,currentX,currentY)) {
-						count++;
-					}
-					currentY++;
+            var count = 0;
+            var self = this;
+            self.visitAdjacentCells(x, y, function(adjacentX, adjacentY){
+                if (self.shouldIncrementNeighborCount(x, y, adjacentX, adjacentY)) {
+					count++;
 				}
-				currentX++;
-			}            
+			});
             return count;
         },
-		trace: function() {
-			var world = this;
-			jQuery.each(world.cells, function(x, row){
+        trace: function(){
+            var world = this;
+            jQuery.each(world.cells, function(x, row){
                 jQuery.each(row, function(y, lives){
                     var neighbors = world.getNeighbors(x, y);
-					jQuery.extend(true, world.cells, neighbors);
+                    jQuery.extend(true, world.cells, neighbors);
                 });
             });
-			this.cells = world.cells;
-		},
-		init: function() {
-			this.cells = {};
-		}
+            this.cells = world.cells;
+        },
+        init: function(){
+            this.cells = {};
+        }
     };
-	WorldObject.prototype = prototype;
-	WorldObject.create = function() {
-		var world = new WorldObject(); 
-			world.init();
-			return world;
-	}
-	return WorldObject
+    WorldObject.prototype = prototype;
+    WorldObject.create = function(){
+        var world = new WorldObject();
+        world.init();
+        return world;
+    }
+    return WorldObject
 });
